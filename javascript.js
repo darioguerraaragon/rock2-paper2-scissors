cfd981e584ac2c8be1500dda6069db7c764e781e
Dario Guerra Aragon <darioguerraaragon@gmail.com>
2022-08-27 17:54:04 -0400

    segundo commit del proyecto , en el proximo commit se le agregara una nueva funcionalidad al proyecto

--- javascript.js
+++ javascript.js
@@ -1,43 +1,54 @@
 const d = document
+let puntos_para_a = 0
+let puntos_para_b = 0
 let btn_play = d.querySelector("#btn_play")
-let btn_stop = d.querySelector("#btn_stop")
+//let btn_stop = d.querySelector("#btn_stop")
 let cards_b = d.querySelectorAll("#cards-b")
 let puntos_a = d.querySelector("#puntos-a")
 let puntos_b = d.querySelector("#puntos-b")
 let imgs = ["paper.png" , "scissors.png" , "stone.png"]
+let img_a = null
 btn_play.addEventListener("click" , e => {
     intervalo = setInterval(repetir_img , 100)
 
     function repetir_img (){
         let cards_a = d.querySelector("#cards-a")
-        var img_a = Math.floor(Math.random()*3)
+        img_a = Math.floor(Math.random()*3)
         cards_a.innerHTML = `<img src=./fotos-proyecto-dos/${[imgs[img_a]]}>`
         //console.log(imgs[img_a])
-        //console.log(img_a) 
-        
-        
+        //console.log(img_a)    
     }
 
 }) 
 
-btn_stop.addEventListener("click" , e =>{
+/*btn_stop.addEventListener("click" , e =>{
     clearInterval(intervalo)
-})
+})*/
 
 cards_b.forEach(el => {
     el.addEventListener("click" , e =>{
         clearInterval(intervalo)
 
-        //console.log(el.dataset.name)
         if(img_a === 0 && el.dataset.name === "piedra"){
-            console.log("perdiste")
+            puntos_para_a += 1
+            puntos_a.innerHTML = `${puntos_para_a}`
         }else if (img_a === 1 && el.dataset.name === "papel"){
-            console.log("perdiste")
-        }else if (img_a === 2 && el.dataset.name === "tijera"){
-            console.log ("perdiste")
+            puntos_para_a += 1
+            puntos_a.innerHTML = `${puntos_para_a}`
+        }else if (img_a === 2 && el.dataset.name === "tijeras"){
+            puntos_para_a += 1
+            puntos_a.innerHTML = `${puntos_para_a}`
+        }else if (img_a === 0 && el.dataset.name === "papel"){
+            console.log("empate")
+        }else if (img_a === 1 && el.dataset.name === "tijeras"){
+            console.log("empate")
+        }else if (img_a === 2 && el.dataset.name === "piedra"){
+            console.log("Empate")
         }else {
-            console.log("Ganaste !!")
+            puntos_para_b += 1
+            puntos_b.innerHTML = `${puntos_para_b}`
         }
+        
         
     })
 })
